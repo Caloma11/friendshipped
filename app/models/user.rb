60759a4da19ef58eb82fe4d29359476bd6134e84
gianluca <perronegian@hotmail.com>
2019-12-01 21:32:26 +0200

    d

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :friendships
+  has_many :friendeds, :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friended_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
+  def friends
+    (friendeds + inverse_friends).uniq
+  end
+
+  def is_friend_with?(user)
+    friends.include?(user)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+  User.create(email: ("a".."z").to_a.sample + "@gmail.com", password: 'password')
+end
+
+
+
+# 100.times do
+# Friendship.create(friended: User.all.sample, user: User.all.sample)
+# end
+
+Friendship.create({
+  friended: User.find(5),
+  user: User.find(6)
+})
+Friendship.create({
+  friended: User.find(5),
+  user: User.find(4)
+})
+Friendship.create({
+  friended: User.find(3),
+  user: User.find(5)
+})
+Friendship.create({
+  friended: User.find(9),
+  user: User.find(5)
+})
